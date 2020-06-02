139f6348f0fc374ebba87b1fb16da9ad7311e521
Кравчук Дмитрий Игоревич <kravchukdi@cbtc.ru>
2020-06-02 17:32:34 +0300

    init projet files

---
+++ src/main/java/extrue/springframework/librarywebapp/LibrarywebappApplication.java
@@ -0,0 +1,13 @@
+package extrue.springframework.librarywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarywebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarywebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/extrue/springframework/librarywebapp/LibrarywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package extrue.springframework.librarywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
