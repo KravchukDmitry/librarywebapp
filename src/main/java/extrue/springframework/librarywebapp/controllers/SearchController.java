dfdcedaf92e8b726cb0c1df2167ec12c139071ee
Кравчук Дмитрий Игоревич <kravchukdi@cbtc.ru>
2020-06-02 21:04:05 +0300

    add restcontroller

---
+++ src/main/java/extrue/springframework/librarywebapp/controllers/SearchController.java
@@ -0,0 +1,23 @@
+package extrue.springframework.librarywebapp.controllers;
+
+import extrue.springframework.librarywebapp.domain.Book;
+import extrue.springframework.librarywebapp.repositories.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class SearchController {
+
+    private final BookRepository bookRepository;
+
+    public SearchController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/restbooks")
+    public Iterable<Book> restBooks() {
+        return bookRepository.findAll();
+    }
+
+}
