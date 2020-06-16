4f611c8ed7817c78a22a258d043ed82c109e29a7
Кравчук Дмитрий Игоревич <kravchukdi@cbtc.ru>
2020-06-16 14:08:57 +0300

    jpa inheritance

--- src/main/java/extrue/springframework/librarywebapp/domain/Author.java
+++ src/main/java/extrue/springframework/librarywebapp/domain/Author.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Author {
 
     @Id
--- src/main/java/extrue/springframework/librarywebapp/domain/Book.java
+++ src/main/java/extrue/springframework/librarywebapp/domain/Book.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Book {
 
     @Id
